a913ac20401a9673775108b8ca9ef2fa67615a9c
PharaohEad <fakhriramadhan233@gmail.com>
2022-09-24 19:34:00 +0700

    For Heroku

--- dto/users/user_request.go
+++ dto/users/user_request.go
@@ -1,21 +1,22 @@
 package usersdto
 
 type CreateUserRequest struct {
-  FullName     	string `json:"fullname" form:"name" validate:"required"`
-  Email    		string `json:"email" form:"email" validate:"required"`
-  Password 		string `json:"password" form:"password" validate:"required"`
-  Gender 		string `json:"gender" form:"gender" validate:"required"`
-  Phone 		string `json:"phone" form:"phone" validate:"required"`
-  Address 		string `json:"address" form:"address" validate:"required"`
-//   Subscribe 	string `json:"subscribe" form:"subscribe" validate:"required"`
+	FullName string `json:"fullname" form:"name" validate:"required"`
+	Email    string `json:"email" form:"email" validate:"required"`
+	Password string `json:"password" form:"password" validate:"required"`
+	Gender   string `json:"gender" form:"gender" validate:"required"`
+	Phone    string `json:"phone" form:"phone" validate:"required"`
+	Address  string `json:"address" form:"address" validate:"required"`
+	//   Subscribe 	string `json:"subscribe" form:"subscribe" validate:"required"`
 }
 
 type UpdateUserRequest struct {
-  FullName     	string `json:"fullname" form:"name"`
-  Email    		string `json:"email" form:"email"`
-  Password		string `json:"password" form:"password"`
-  Gender 		string `json:"gender" form:"gender" validate:"required"`
-  Phone 		string `json:"phone" form:"phone" validate:"required"`
-  Address 		string `json:"address" form:"address" validate:"required"`
-//   Subscribe 	string `json:"subscribe" form:"subscribe" validate:"required"`
+	FullName string `json:"fullname" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
+	Gender   string `json:"gender" form:"gender"`
+	Phone    string `json:"phone" form:"phone"`
+	Address  string `json:"address" form:"address"`
+	Status   string `json:"status"`
+	//   Subscribe 	string `json:"subscribe" form:"subscribe" validate:"required"`
 }
